a0f5d2bf8a74e55e935556d090ce339278ef7fe4
Henrique Pötter <henriquepotter.hp@gmail.com>
2019-06-18 11:09:09 -0400

    LMF

--- pi_face_detection.py
+++ pi_face_detection.py
@@ -104,7 +104,7 @@ class PiFaceDet:
         frame_as_string = None
 
         if np.any(face_drawn_frame):
-            frame_as_string = frame_as_string.tostring()
+            frame_as_string = face_drawn_frame.tostring()
 
         return most_similar_name, frame_as_string
 
