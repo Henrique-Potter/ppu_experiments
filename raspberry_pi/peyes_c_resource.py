cc8d1adf907101823192b4a6b97b9fc56eafda86
Henrique Pötter <henriquepotter.hp@gmail.com>
2019-07-16 18:07:38 -0400

    Added beep to get function.

--- raspberry_pi/peyes_c_resource.py
+++ raspberry_pi/peyes_c_resource.py
@@ -5,11 +5,16 @@ from coapthon.resources.resource import Resource
 from multiprocessing import Process
 from pi_face_detection import PiFaceDet
 from threading import Lock
+import platform
 
 peyes_lock = Lock()
 inputQueue = Queue(maxsize=5)
 
 continuous_server = False
+
+beep_pin = 40
+g_led_pin = 36
+r_led_pin = 38
 
 
 def start_face_det(learn_face_count):
@@ -35,12 +40,14 @@ class PeyesC(Resource):
         global continuous_server
 
         if not continuous_server:
+            self.beep_blink(3, r_led_pin, 0.5)
             p = Process(target=start_face_det, args=(inputQueue,))
             p.daemon = True
             p.start()
             continuous_server = True
             self.payload = "ID process started Successfully!"
         else:
+            self.beep_blink(1, g_led_pin, 0.2)
             inputQueue.put(1)
             self.payload = "Trigger Sensor sent a get request!"
             self.payload = "Continuous ID process is already ON!"
@@ -73,4 +80,17 @@ class PeyesC(Resource):
     def render_DELETE(self, request):
         return True
 
+    @staticmethod
+    def beep_blink(blink_times, led_pin, duration=0.3):
+        import RPi.GPIO as GPIO
+        import time
+
+        if platform.uname()[1] == 'raspberrypi':
+            for i in range(blink_times):
+                GPIO.output(beep_pin, GPIO.HIGH)
+                GPIO.output(led_pin, GPIO.HIGH)
+                time.sleep(duration)
+                GPIO.output(beep_pin, GPIO.LOW)
+                GPIO.output(led_pin, GPIO.LOW)
+                time.sleep(duration)
 
