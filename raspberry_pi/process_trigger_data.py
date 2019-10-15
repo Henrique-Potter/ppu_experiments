5045e87c2cc29ae6239159cc72c3e3eec3214aa5
Henrique Pötter <henriquepotter.hp@gmail.com>
2019-10-15 18:47:25 -0400

    Removing unecessary csv files and Code refactoring.

--- raspberry_pi/process_trigger_data.py
+++ raspberry_pi/process_trigger_data.py
@@ -6,17 +6,14 @@ import numpy as np
 trigger_data = None
 trigger_data2 = None
 
-if os.path.exists("trigger_metrics_a.csv"):
-    trigger_data = pd.read_csv("trigger_metrics_a.csv", index_col=False).values
+if os.path.exists("trigger_metrics.csv"):
+    trigger_data = pd.read_csv("trigger_metrics.csv", index_col=False).values
 
-if os.path.exists("trigger_metrics_2_a.csv"):
-    trigger_data2 = pd.read_csv("trigger_metrics_2_a.csv", index_col=False).values
-
-trigger_data = np.concatenate((trigger_data, trigger_data2), axis=0)
+ignored_trigger=2
+target_trigger=1
+trigger_data = trigger_data[trigger_data[:, 0] != ignored_trigger, :]
 
 trigger_data = trigger_data[trigger_data[:, 1].argsort()]
-
-trigger_data = trigger_data[trigger_data[:, 0] != 0, :]
 
 tp_s_trigger = 0
 fp_s_trigger = 0
@@ -33,12 +30,12 @@ for i in range(log_size):
     b_distance = 6
     a_distance = 6
 
-    if trigger_data[i, 0] == 1:
+    if trigger_data[i, 0] == 0:
         total_cam_triggers += 1
-        if i - 1 >= 0 and trigger_data[i - 1, 0] == 2:
+        if i - 1 >= 0 and trigger_data[i - 1, 0] == target_trigger:
             b_distance = abs(trigger_data[i - 1, 1] - trigger_data[i, 1])
 
-        if i + 1 < log_size and trigger_data[i + 1, 0] == 2:
+        if i + 1 < log_size and trigger_data[i + 1, 0] == target_trigger:
             a_distance = abs(trigger_data[i + 1, 1] - trigger_data[i, 1])
 
         if b_distance <= time_window or a_distance < time_window:
@@ -48,15 +45,14 @@ for i in range(log_size):
 
     else:
         total_sen_triggers += 1
-        if i - 1 >= 0 and trigger_data[i - 1, 0] == 1:
+        if i - 1 >= 0 and trigger_data[i - 1, 0] == 0:
             b_distance = abs(trigger_data[i - 1, 1] - trigger_data[i, 1])
 
-        if i + 1 < log_size and trigger_data[i + 1, 0] == 1:
+        if i + 1 < log_size and trigger_data[i + 1, 0] == 0:
             a_distance = abs(trigger_data[i + 1, 1] - trigger_data[i, 1])
 
         if b_distance > time_window and a_distance > time_window:
             fp_s_trigger += 1
-
 
 
 print("Total cam triggers: {}".format(total_cam_triggers))
